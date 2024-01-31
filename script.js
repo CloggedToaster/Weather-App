0a272c0a57345c5fe54411d35bfe1ebac81ce80e
cloggedtoaster <jjavier848@outlook.com>
2024-01-30 21:18:39 -0600

    Added a function to fetch all data in an easy to parse manner

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const fetchLocation = async(cityName) => {
     
     const apiResponse = await fetch(apiQuery);
     const json = await apiResponse.json();
-
+    
+    if(!json.results){
+        return {
+            "error": true,
+        };
+    }
+    
     let latitude = json.results[0].latitude;
     let longitude = json.results[0].longitude;
     let country = json.results[0].country;
@@ -18,7 +24,7 @@ const fetchLocation = async(cityName) => {
         "latitude": latitude, 
         "longitude": longitude, 
         "cityName": cityName, 
-        "location": location
+        "location": location,
     };
 }
 
@@ -44,16 +50,26 @@ const fetchWeather = async(latitude, longitude) => {
     let cloudCoverageUnit = json.current_units.cloud_cover;
 
     return {
-        "temperature":temperature+temperatureUnit,
-        "weatherCode":weatherCodeUnit+" "+weatherCode,
-        "cloudCoverage":cloudCoverage+cloudCoverageUnit
+        "temperature":temperature,
+        "temperatureString":temperature+temperatureUnit,
+        "weatherCode":weatherCode,
+        "weatherCodeString":weatherCodeUnit+" "+weatherCode,
+        "cloudCoverage":cloudCoverage,
+        "cloudCoverageString":cloudCoverage+cloudCoverageUnit,
     };
 }
 
-const printCityData = async (cityName) => {
+const printCityData = async () => {
+    const cityName = document.getElementById("cityField").value;
     fetchLocation(cityName)
     .then(locationResponse => {
-        fetchWeather(locationResponse.latitude, locationResponse.longitude).then(weatherResponse => {
+        if(locationResponse.error){
+            return{
+                "error": true,
+            };
+        }
+        fetchWeather(locationResponse.latitude, locationResponse.longitude)
+        .then(weatherResponse => {
             console.log(weatherResponse.temperature);
             console.log(weatherResponse.weatherCode);
             console.log("Cloud Coverage: "+weatherResponse.cloudCoverage);
@@ -64,11 +80,63 @@ const printCityData = async (cityName) => {
         
         console.log(latitudeString+", "+longitudeString);
         console.log(locationString);
+        return{
+            "error": false,
+        };
+    })
+    .then(response => {
+        if(response.error){
+            console.log("City does not exist!");
+        }
     });
 }
 
-printCityData("Melbourne");
-// printCityData("Newark");
-// printCityData("London");
-// printCityData("Tokyo");
-// printCityData("Beijing");
+const fetchAllData = async (cityName) => {
+    let data = {};
+    const locationData = await fetchLocation(cityName);
+    const weatherData = await fetchWeather(locationData.latitude, locationData.longitude);
+
+    weatherData.latitudeString = Math.abs(locationData.latitude)+(locationData.latitude>0?"° N":"° S");
+    weatherData.longitudeString = Math.abs(locationData.longitude)+(locationData.longitude>0?"° E":"° W");
+    weatherData.locationString = cityName+", "+locationData.location;
+
+    data = {...locationData, ...weatherData};
+    console.log(data)
+    return data;
+}
+
+// Event Listeners
+window.addEventListener("DOMContentLoaded", (event) => {
+    const cityForm = document.getElementById("cityForm");
+    const cityField = document.getElementById("cityField");
+    if(cityForm) {
+        cityForm.addEventListener("submit", handleFormSubmit, false);
+    }
+});
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    fetchAllData(cityField.value)
+    .then(res => {
+
+        const cityNameDiv = document.getElementById("cityNameDiv");
+        
+        const locationHeader = document.createElement("h2");
+        const temperatureHeader = document.createElement("h3");
+        const latitudeHeader = document.createElement("h3");
+        const longitudeHeader = document.createElement("h3");
+        
+        locationHeader.innerText = res.locationString;
+        temperatureHeader.innerText = res.temperatureString;
+        latitudeHeader.innerText = res.latitudeString;
+        longitudeHeader.innerText = res.longitudeString;
+
+        cityNameDiv.appendChild(locationHeader)
+        cityNameDiv.appendChild(temperatureHeader)
+        cityNameDiv.appendChild(latitudeHeader)
+        cityNameDiv.appendChild(longitudeHeader)
+    })
+    
+}
+  
